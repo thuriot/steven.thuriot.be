ae71ddc8fc07b4400bf9252a4004b4505e3aa568
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-06-25 19:43:56 +0200

    Reformatted deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,22 @@ rev=$(git rev-parse --short HEAD)
 cd _site
 
 git init
-git config user.name $GIT_NAME
-git config user.email $GIT_EMAIL
+git config user.name "$GIT_NAME"
+git config user.email "$GIT_EMAIL"
 
 git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
 git fetch upstream && git reset upstream/gh-pages
 
 # echo "steven.thuriot.be" > CNAME
 
+
 touch .
 
+
 git add -A .
+
+
 git commit -m "TravisCI: Rebuild pages at ${rev} - Build #$TRAVIS_BUILD_NUMBER"
+
+
 git push -q upstream HEAD:gh-pages

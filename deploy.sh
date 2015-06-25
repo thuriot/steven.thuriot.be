b38e93dc575f8ff59ec8de8c7b2bfa1469c46993
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-06-25 19:45:39 +0200

    Added enter

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ git config user.name "$GIT_NAME"
 git config user.email "$GIT_EMAIL"
 
 git remote add upstream "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+
 git fetch upstream && git reset upstream/gh-pages
 
 # echo "steven.thuriot.be" > CNAME

54018eb0541a04da4363aad77f3796377b300b3c
Steven Jack Thuriot <steven@thuriot.be>
2021-12-03 12:02:28 +0100

    Added Dockerfile to generate the website

---
+++ generate.ps1
@@ -0,0 +1,23 @@
+# docker build -t build-steventhuriot:1.0 .
+docker run -v ${pwd}:/usr/src/app -it build-steventhuriot:1.0
+
+cd _site
+
+Remove-Item build.sh -Force
+Remove-Item Dockerfile -Force
+Remove-Item generate.ps1 -Force
+
+git init
+git remote add upstream "https://github.com/thuriot/steven.thuriot.be.git"
+
+git fetch upstream && git reset upstream/gh-pages
+
+echo $null >> .nojekyll
+echo "steven.thuriot.be" > CNAME
+
+git add -A .
+git commit -m "Rebuild pages"
+git push -q upstream HEAD:gh-pages
+
+cd ..
+Remove-Item -LiteralPath _site -Force -Recurse

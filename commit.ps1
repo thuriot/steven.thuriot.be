2468f74906bb93be7e3c7473341571a67947d872
Steven Jack Thuriot <steven@thuriot.be>
2021-12-12 14:44:41 +0100

    Updated website

--- commit.ps1
+++ commit.ps1
@@ -1,7 +1,7 @@
 # docker build -t build-steventhuriot:1.0 .
-docker run -v ${pwd}:/usr/src/app --rm -it build-steventhuriot:1.0
+docker run --name BuildJekyll -v ${pwd}:/usr/src/app --rm -it build-steventhuriot:1.0
 
-cd _site
+Set-Location _site
 
 Remove-Item build.sh -Force
 Remove-Item Dockerfile -Force
@@ -12,14 +12,14 @@ git remote add upstream "https://github.com/thuriot/steven.thuriot.be.git"
 
 git fetch upstream && git reset upstream/gh-pages
 
-echo $null >> .nojekyll
-echo "steven.thuriot.be" > CNAME
+Write-Output $null >> .nojekyll
+Write-Output "steven.thuriot.be" > CNAME
 
 git add -A .
 git commit -m "Rebuild pages"
 git push -q upstream HEAD:gh-pages
 
-cd ..
+Set-Location ..
 Remove-Item -LiteralPath _site -Force -Recurse
 
 git add -A .
